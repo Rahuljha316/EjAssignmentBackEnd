c1f777b22b53238050fa838fbcddda44fae8142e
Rahul Jha <rahulbudy11@gmail.com>
2023-03-23 11:47:45 +0530

    initialised the project

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const { Sequelize } = require('sequelize');
+
+
+const sequelize = new Sequelize('ejamerica', 'root', '',{
+    host:'localhost',
+    dialect:'mysql'
+
+})
+
+try{
+    sequelize.authenticate()
+    console.log('Connection have been establised successfully.');
+}catch(error){
+    console.error('Unable to connect to database',error);
+}
